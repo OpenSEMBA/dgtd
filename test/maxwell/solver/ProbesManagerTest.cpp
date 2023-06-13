70f2ed0c6a1a80c576e90abcae58e44dbfdd1bdd
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2023-06-13 10:07:41 +0200

    Field Probe Test

--- test/maxwell/solver/ProbesManagerTest.cpp
+++ test/maxwell/solver/ProbesManagerTest.cpp
@@ -4,6 +4,7 @@
 
 #include "solver/ProbesManager.h"
 #include "solver/SourcesManager.h"
+#include <solver/Solver.h>
 
 using namespace maxwell;
 using namespace mfem;
@@ -28,4 +29,25 @@ TEST_F(ProbesManagerTest, exporterProbe)
 	ProbesManager pM{ ps, fes, fields, SolverOptions{} };
 
 	ASSERT_NO_THROW(pM.updateProbes(0.0));
+}
+
+TEST_F(ProbesManagerTest, fieldProbe)
+{
+	Mesh m{ Mesh::MakeCartesian1D(5) };
+	DG_FECollection fec{ 2, 1, BasisType::GaussLobatto };
+	FiniteElementSpace fes{ &m, &fec };
+	Fields fields{ fes };
+	SourcesManager sM{ buildGaussianInitialField(), fes };
+	sM.setInitialFields(fields);
+
+	Probes probes;
+	probes.fieldProbes = {
+		FieldProbe{{0.5}}
+	};
+
+	ProbesManager pM{ probes, fes, fields, SolverOptions{} };
+
+	ASSERT_NO_THROW(pM.updateProbes(0.0));
+	EXPECT_NO_THROW(pM.getFieldProbe(0));
+	EXPECT_TRUE(pM.getFieldProbe(0).getFieldMovies().at(0).Ex);
 }
