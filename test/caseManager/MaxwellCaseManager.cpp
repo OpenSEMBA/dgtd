fc14d0e0661c40af23cabdc0336944b1e1f61bd2
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2023-10-31 08:43:42 +0100

    Formatting

--- test/caseManager/MaxwellCaseManager.cpp
+++ test/caseManager/MaxwellCaseManager.cpp
@@ -8,9 +8,9 @@ Solver assembleCaseSolver(std::string case_name)
 	auto file_name{ maxwellCase("1D_PEC_Centered") };
 	std::ifstream test_file(file_name);
 	auto case_data = json::parse(test_file);
-	Model model{ assembleModel(case_data) };
-	Probes probes{ assembleProbes(case_data) };
-	Sources sources{ assembleSources(case_data) };
+	Model model             { assembleModel        (case_data) };
+	Probes probes           { assembleProbes       (case_data) };
+	Sources sources         { assembleSources      (case_data) };
 	SolverOptions solverOpts{ assembleSolverOptions(case_data) };
 
 	return Solver(model, probes, sources, solverOpts);
