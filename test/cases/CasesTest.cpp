b7b1ee45886302b2af0e25c3cca185a6a0ca4018
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2024-08-14 13:59:20 +0800

    Fixes Cases and relevant .json to pass tests

--- test/cases/CasesTest.cpp
+++ test/cases/CasesTest.cpp
@@ -16,7 +16,7 @@ TEST_F(CasesTest, 1D_PEC_Upwind)
 {
 
 	std::string case_name{ "1D_PEC" };
-	auto solver{ buildSolver(case_name) };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	GridFunction eOld{ solver.getField(E,Y) };
 	auto normOld{ solver.getFields().getNorml2() };
@@ -73,139 +73,11 @@ TEST_F(CasesTest, 1D_PEC_Upwind)
 
 }
 
-TEST_F(CasesTest, 2D_PEC_Centered)
-{
-	auto data{ parseJSONfile("2D_PEC") };
-	data["solver_options"]["solver_type"] = "centered";
-	auto solver{ buildSolver(data) };
-
-	GridFunction eOld{ solver.getField(E,Z) };
-	auto normOld{ solver.getFields().getNorml2() };
-
-	// Checks fields have been initialized.
-	EXPECT_NE(0.0, normOld);
-
-	double tolerance{ 2e-2 };
-
-	solver.run();
-
-	// Checks that field is almost the same as initially because the completion 
-	// of a cycle.
-	GridFunction eNew{ solver.getField(E,Z) };
-	EXPECT_NEAR(0.0, eOld.DistanceTo(eNew), tolerance);
-
-	// Compares all DOFs.
-	EXPECT_NEAR(normOld, solver.getFields().getNorml2(), 1e-3);
-
-	// At the left boundary and right boundaries the electric field should be always close to zero 
-	// while the magnetic field should reach +- 2.0 at specific times...
-	{
-		auto expected_t_half{ 0.5 };
-		for (const auto& [t, f] : solver.getFieldProbe(0).getFieldMovies()) {
-			EXPECT_NEAR(0.0, f.Ex, tolerance);
-			EXPECT_NEAR(0.0, f.Ey, tolerance);
-			EXPECT_NEAR(0.0, f.Ez, tolerance);
-			if (abs(t - expected_t_half) <= 1e-3) {
-				EXPECT_NEAR(2.0, f.Hy, tolerance);
-			}
-		}
-
-		for (const auto& [t, f] : solver.getFieldProbe(1).getFieldMovies()) {
-			EXPECT_NEAR(0.0, f.Ex, tolerance);
-			EXPECT_NEAR(0.0, f.Ey, tolerance);
-			EXPECT_NEAR(0.0, f.Ez, tolerance);
-			if (abs(t - expected_t_half) <= 1e-3) {
-				EXPECT_NEAR(2.0, f.Hy, tolerance);
-			}
-		}
-	}
-
-	//...and at the start and end of the simulation, in the center of the problem,
-	// the electric field should be always close to 1.0 due to dimensions of the box.
-
-	for (const auto& [t, f] : solver.getFieldProbe(2).getFieldMovies()) {
-		auto expected_t_initial{ 0.0 };
-		auto expected_t_final{ 2.0 };
-		if (abs(t - expected_t_initial) <= 1e-3) {
-			EXPECT_NEAR(1.0, f.Ez, tolerance);
-			EXPECT_NEAR(0.0, f.Hy, tolerance);
-		}
-		if (abs(t - expected_t_final) <= 1e-3) {
-			EXPECT_NEAR(1.0, f.Ez, tolerance);
-			EXPECT_NEAR(0.0, f.Hy, tolerance);
-		}
-	}
-
-}
-
-TEST_F(CasesTest, 2D_PEC_Upwind)
-{
-	std::string case_data {"2D_PEC"};
-	auto solver{ buildSolver(case_data) };
-
-	GridFunction eOld{ solver.getField(E,Z) };
-	auto normOld{ solver.getFields().getNorml2() };
-
-	// Checks fields have been initialized.
-	EXPECT_NE(0.0, normOld);
-
-	double tolerance{ 2e-2 };
-
-	solver.run();
-
-	// Checks that field is almost the same as initially because the completion 
-	// of a cycle.
-	GridFunction eNew{ solver.getField(E,Z) };
-	EXPECT_NEAR(0.0, eOld.DistanceTo(eNew), tolerance);
-
-	// Compares all DOFs.
-	EXPECT_NEAR(normOld, solver.getFields().getNorml2(), 5e-3);
-
-	// At the left boundary and right boundaries the electric field should be always close to zero 
-	// while the magnetic field should reach +- 2.0 at specific times...
-	{
-		auto expected_t_half{ 0.5 };
-		for (const auto& [t, f] : solver.getFieldProbe(0).getFieldMovies()) {
-			EXPECT_NEAR(0.0, f.Ex, tolerance);
-			EXPECT_NEAR(0.0, f.Ey, tolerance);
-			EXPECT_NEAR(0.0, f.Ez, tolerance);
-			if (abs(t - expected_t_half) <= 1e-3) {
-				EXPECT_NEAR(2.0, f.Hy, tolerance);
-			}
-		}
-
-		for (const auto& [t, f] : solver.getFieldProbe(1).getFieldMovies()) {
-			EXPECT_NEAR(0.0, f.Ex, tolerance);
-			EXPECT_NEAR(0.0, f.Ey, tolerance);
-			EXPECT_NEAR(0.0, f.Ez, tolerance);
-			if (abs(t - expected_t_half) <= 1e-3) {
-				EXPECT_NEAR(2.0, f.Hy, tolerance);
-			}
-		}
-	}
-	//...and at the start and end of the simulation, in the center of the problem,
-	// the electric field should be always close to 1.0 due to dimensions of the box.
-
-	for (const auto& [t, f] : solver.getFieldProbe(2).getFieldMovies()) {
-		auto expected_t_initial{ 0.0 };
-		auto expected_t_final{ 2.0 };
-		if (abs(t - expected_t_initial) <= 1e-3) {
-			EXPECT_NEAR(1.0, f.Ez, tolerance);
-			EXPECT_NEAR(0.0, f.Hy, tolerance);
-		}
-		if (abs(t - expected_t_final) <= 1e-3) {
-			EXPECT_NEAR(1.0, f.Ez, tolerance);
-			EXPECT_NEAR(0.0, f.Hy, tolerance);
-		}
-	}
-
-}
-
 TEST_F(CasesTest, 1D_TFSF_Centered)
 {
-	auto data{ parseJSONfile("1D_TFSF") };
-	data["solver_options"]["solver_type"] = "centered";
-	auto solver{ buildSolver(data) };
+	auto case_data{ parseJSONfile(maxwellCase("1D_TFSF")) };
+	case_data["solver_options"]["solver_type"] = "centered";
+	auto solver{ buildSolver(case_data) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -275,8 +147,8 @@ TEST_F(CasesTest, 1D_TFSF_Centered)
 
 TEST_F(CasesTest, 1D_TFSF_Upwind_TEy)
 {
-	std::string case_data{ "1D_TFSF_TEy" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "1D_TFSF_TEy" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -308,7 +180,7 @@ TEST_F(CasesTest, 1D_TFSF_Upwind_TEy)
 			EXPECT_NEAR(0.0, f.Hy, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(0.0, f.Ey, tolerance);
-				EXPECT_NEAR(-2.0, f.Hz, tolerance);
+				EXPECT_NEAR(2.0, f.Hz, tolerance);
 			}
 		}
 	}
@@ -322,7 +194,7 @@ TEST_F(CasesTest, 1D_TFSF_Upwind_TEy)
 			EXPECT_NEAR(0.0, f.Hy, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(-1.0, f.Ey, tolerance);
-				EXPECT_NEAR(-1.0, f.Hz, tolerance);
+				EXPECT_NEAR( 1.0, f.Hz, tolerance);
 			}
 		}
 	}
@@ -344,8 +216,8 @@ TEST_F(CasesTest, 1D_TFSF_Upwind_TEy)
 
 TEST_F(CasesTest, 1D_TFSF_Upwind_THz)
 {
-	std::string case_data{ "1D_TFSF_THz" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "1D_TFSF_THz" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -376,8 +248,8 @@ TEST_F(CasesTest, 1D_TFSF_Upwind_THz)
 			EXPECT_NEAR(0.0, f.Hx, tolerance);
 			EXPECT_NEAR(0.0, f.Hy, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
-				EXPECT_NEAR( 1.0, f.Ey, tolerance);
-				EXPECT_NEAR( 1.0, f.Hz, tolerance);
+				EXPECT_NEAR(1.0, f.Ey, tolerance);
+				EXPECT_NEAR(1.0, f.Hz, tolerance);
 			}
 		}
 	}
@@ -391,7 +263,7 @@ TEST_F(CasesTest, 1D_TFSF_Upwind_THz)
 			EXPECT_NEAR(0.0, f.Hy, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(-1.0, f.Ey, tolerance);
-				EXPECT_NEAR( 1.0, f.Hz, tolerance);
+				EXPECT_NEAR(1.0, f.Hz, tolerance);
 			}
 		}
 	}
@@ -412,11 +284,140 @@ TEST_F(CasesTest, 1D_TFSF_Upwind_THz)
 
 }
 
+
+TEST_F(CasesTest, 2D_PEC_Centered)
+{
+	auto case_data{ parseJSONfile(maxwellCase("2D_PEC")) };
+	case_data["solver_options"]["solver_type"] = "centered";
+	auto solver{ buildSolver(case_data) };
+
+	GridFunction eOld{ solver.getField(E,Z) };
+	auto normOld{ solver.getFields().getNorml2() };
+
+	// Checks fields have been initialized.
+	EXPECT_NE(0.0, normOld);
+
+	double tolerance{ 2e-2 };
+
+	solver.run();
+
+	// Checks that field is almost the same as initially because the completion 
+	// of a cycle.
+	GridFunction eNew{ solver.getField(E,Z) };
+	EXPECT_NEAR(0.0, eOld.DistanceTo(eNew), tolerance);
+
+	// Compares all DOFs.
+	EXPECT_NEAR(normOld, solver.getFields().getNorml2(), 1e-3);
+
+	// At the left boundary and right boundaries the electric field should be always close to zero 
+	// while the magnetic field should reach +- 2.0 at specific times...
+	{
+		auto expected_t_half{ 0.5 };
+		for (const auto& [t, f] : solver.getFieldProbe(0).getFieldMovies()) {
+			EXPECT_NEAR(0.0, f.Ex, tolerance);
+			EXPECT_NEAR(0.0, f.Ey, tolerance);
+			EXPECT_NEAR(0.0, f.Ez, tolerance);
+			if (abs(t - expected_t_half) <= 1e-3) {
+				EXPECT_NEAR(1.0, f.Hy, tolerance);
+			}
+		}
+
+		for (const auto& [t, f] : solver.getFieldProbe(1).getFieldMovies()) {
+			EXPECT_NEAR(0.0, f.Ex, tolerance);
+			EXPECT_NEAR(0.0, f.Ey, tolerance);
+			EXPECT_NEAR(0.0, f.Ez, tolerance);
+			if (abs(t - expected_t_half) <= 1e-3) {
+				EXPECT_NEAR(-1.0, f.Hy, tolerance);
+			}
+		}
+	}
+
+	//...and at the start and end of the simulation, in the center of the problem,
+	// the electric field should be always close to 1.0 due to dimensions of the box.
+
+	for (const auto& [t, f] : solver.getFieldProbe(2).getFieldMovies()) {
+		auto expected_t_initial{ 0.0 };
+		auto expected_t_final{ 2.0 };
+		if (abs(t - expected_t_initial) <= 1e-3) {
+			EXPECT_NEAR(1.0, f.Ez, tolerance);
+			EXPECT_NEAR(0.0, f.Hy, tolerance);
+		}
+		if (abs(t - expected_t_final) <= 1e-3) {
+			EXPECT_NEAR(1.0, f.Ez, tolerance);
+			EXPECT_NEAR(0.0, f.Hy, tolerance);
+		}
+	}
+
+}
+
+TEST_F(CasesTest, 2D_PEC_Upwind)
+{
+	std::string case_name{"2D_PEC"};
+	auto solver{ buildSolver(maxwellCase(case_name)) };
+
+	GridFunction eOld{ solver.getField(E,Z) };
+	auto normOld{ solver.getFields().getNorml2() };
+
+	// Checks fields have been initialized.
+	EXPECT_NE(0.0, normOld);
+
+	double tolerance{ 2e-2 };
+
+	solver.run();
+
+	// Checks that field is almost the same as initially because the completion 
+	// of a cycle.
+	GridFunction eNew{ solver.getField(E,Z) };
+	EXPECT_NEAR(0.0, eOld.DistanceTo(eNew), tolerance);
+
+	// Compares all DOFs.
+	EXPECT_NEAR(normOld, solver.getFields().getNorml2(), 5e-3);
+
+	// At the left boundary and right boundaries the electric field should be always close to zero 
+	// while the magnetic field should reach +- 2.0 at specific times...
+	{
+		auto expected_t_half{ 0.5 };
+		for (const auto& [t, f] : solver.getFieldProbe(0).getFieldMovies()) {
+			EXPECT_NEAR(0.0, f.Ex, tolerance);
+			EXPECT_NEAR(0.0, f.Ey, tolerance);
+			EXPECT_NEAR(0.0, f.Ez, tolerance);
+			if (abs(t - expected_t_half) <= 1e-3) {
+				EXPECT_NEAR(1.0, f.Hy, tolerance);
+			}
+		}
+
+		for (const auto& [t, f] : solver.getFieldProbe(1).getFieldMovies()) {
+			EXPECT_NEAR(0.0, f.Ex, tolerance);
+			EXPECT_NEAR(0.0, f.Ey, tolerance);
+			EXPECT_NEAR(0.0, f.Ez, tolerance);
+			if (abs(t - expected_t_half) <= 1e-3) {
+				EXPECT_NEAR(-1.0, f.Hy, tolerance);
+			}
+		}
+	}
+	//...and at the start and end of the simulation, in the center of the problem,
+	// the electric field should be always close to 1.0 due to dimensions of the box.
+
+	for (const auto& [t, f] : solver.getFieldProbe(2).getFieldMovies()) {
+		auto expected_t_initial{ 0.0 };
+		auto expected_t_final{ 2.0 };
+		if (abs(t - expected_t_initial) <= 1e-3) {
+			EXPECT_NEAR(1.0, f.Ez, tolerance);
+			EXPECT_NEAR(0.0, f.Hy, tolerance);
+		}
+		if (abs(t - expected_t_final) <= 1e-3) {
+			EXPECT_NEAR(1.0, f.Ez, tolerance);
+			EXPECT_NEAR(0.0, f.Hy, tolerance);
+		}
+	}
+
+}
+
 TEST_F(CasesTest, 2D_TFSF_Centered)
 {
-	auto data{ parseJSONfile("2D_TFSF_TEy") };
-	data["solver_options"]["solver_type"] = "centered";
-	auto solver{ buildSolver(data) };
+	auto case_data{ parseJSONfile(maxwellCase("2D_TFSF_TEy")) };
+	case_data["solver_options"]["solver_type"] = "centered";
+	auto solver{ buildSolver(case_data) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -485,8 +486,8 @@ TEST_F(CasesTest, 2D_TFSF_Centered)
 
 TEST_F(CasesTest, 2D_TFSF_Upwind_TEy)
 {
-	std::string case_data{ "2D_TFSF_TEy" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_TFSF_TEy" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -555,8 +556,8 @@ TEST_F(CasesTest, 2D_TFSF_Upwind_TEy)
 
 TEST_F(CasesTest, 2D_TFSF_Upwind_THz)
 {
-	std::string case_data{ "2D_TFSF_THz" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_TFSF_THz" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -623,59 +624,59 @@ TEST_F(CasesTest, 2D_TFSF_Upwind_THz)
 
 }
 
-TEST_F(CasesTest, 2D_NTFF_Box_Upwind)
+TEST_F(CasesTest, DISABLED_2D_NTFF_Box_Upwind) //Unsupported Gmsh element type.
 {
-	std::string case_data{ "2D_NTFF_Box" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_NTFF_Box" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 2D_RCS_SubLambda_Hz)
 {
-	std::string case_data{ "2D_RCS" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_RCS" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 2D_RCS_ThreeLambda_Hz)
 {
-	std::string case_data{ "2D_RCS_ThreeLambda" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_RCS_ThreeLambda" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 2D_RCS_Salva_Hz)
 {
-	std::string case_data{ "2D_RCS_Salva" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_RCS_Salva" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 2D_RCS_SalvaConfig_Hz)
 {
-	std::string case_data{ "2D_RCS_SalvaConfig" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_RCS_SalvaConfig" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 2D_PEC_Bounce45_Upwind)
 {
-	std::string case_data{ "2D_PEC_Bounce" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "2D_PEC_Bounce" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 3D_TFSF_Centered)
 {
-	auto data{ parseJSONfile("3D_TFSF") };
-	data["solver_options"]["solver_type"] = "centered";
-	auto solver{ buildSolver(data) };
+	auto case_data{ parseJSONfile(maxwellCase("3D_TFSF")) };
+	case_data["solver_options"]["solver_type"] = "centered";
+	auto solver{ buildSolver(case_data) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -693,7 +694,7 @@ TEST_F(CasesTest, 3D_TFSF_Centered)
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(0.0, f.Ez, tolerance);
-				EXPECT_NEAR(0.5, f.Hy, tolerance);
+				EXPECT_NEAR(-0.5, f.Hy, tolerance);
 			}
 		}
 	}
@@ -706,8 +707,8 @@ TEST_F(CasesTest, 3D_TFSF_Centered)
 			EXPECT_NEAR(0.0, f.Hx, tolerance);
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
-				EXPECT_NEAR(-1.0, f.Ez, tolerance);
-				EXPECT_NEAR(1.0, f.Hy, tolerance);
+				EXPECT_NEAR(1.0, f.Ez, tolerance);
+				EXPECT_NEAR(-1.0, f.Hy, tolerance);
 			}
 		}
 	}
@@ -720,8 +721,8 @@ TEST_F(CasesTest, 3D_TFSF_Centered)
 			EXPECT_NEAR(0.0, f.Hx, tolerance);
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
-				EXPECT_NEAR(1.0, f.Ez, tolerance);
-				EXPECT_NEAR(1.0, f.Hy, tolerance);
+				EXPECT_NEAR(-1.0, f.Ez, tolerance);
+				EXPECT_NEAR(-1.0, f.Hy, tolerance);
 			}
 		}
 	}
@@ -735,7 +736,7 @@ TEST_F(CasesTest, 3D_TFSF_Centered)
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(0.0, f.Ez, tolerance);
-				EXPECT_NEAR(2.0, f.Hy, tolerance);
+				EXPECT_NEAR(-2.0, f.Hy, tolerance);
 			}
 		}
 	}
@@ -744,8 +745,8 @@ TEST_F(CasesTest, 3D_TFSF_Centered)
 
 TEST_F(CasesTest, 3D_TFSF_Upwind)
 {
-	std::string case_data{ "3D_TFSF" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "3D_TFSF" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	auto normOld{ solver.getFields().getNorml2() };
 	EXPECT_EQ(0.0, normOld);
@@ -763,7 +764,7 @@ TEST_F(CasesTest, 3D_TFSF_Upwind)
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(0.0, f.Ez, tolerance);
-				EXPECT_NEAR(0.5, f.Hy, tolerance);
+				EXPECT_NEAR(-0.5, f.Hy, tolerance);
 			}
 		}
 	}
@@ -776,8 +777,8 @@ TEST_F(CasesTest, 3D_TFSF_Upwind)
 			EXPECT_NEAR(0.0, f.Hx, tolerance);
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
-				EXPECT_NEAR(-1.0, f.Ez, tolerance);
-				EXPECT_NEAR( 1.0, f.Hy, tolerance);
+				EXPECT_NEAR(1.0, f.Ez, tolerance);
+				EXPECT_NEAR(-1.0, f.Hy, tolerance);
 			}
 		}
 	}
@@ -790,8 +791,8 @@ TEST_F(CasesTest, 3D_TFSF_Upwind)
 			EXPECT_NEAR(0.0, f.Hx, tolerance);
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
-				EXPECT_NEAR(1.0, f.Ez, tolerance);
-				EXPECT_NEAR(1.0, f.Hy, tolerance);
+				EXPECT_NEAR(-1.0, f.Ez, tolerance);
+				EXPECT_NEAR(-1.0, f.Hy, tolerance);
 			}
 		}
 	}
@@ -805,7 +806,7 @@ TEST_F(CasesTest, 3D_TFSF_Upwind)
 			EXPECT_NEAR(0.0, f.Hz, tolerance);
 			if (abs(t - expected_t) <= 1e-3) {
 				EXPECT_NEAR(0.0, f.Ez, tolerance);
-				EXPECT_NEAR(2.0, f.Hy, tolerance);
+				EXPECT_NEAR(-2.0, f.Hy, tolerance);
 			}
 		}
 	}
@@ -814,24 +815,24 @@ TEST_F(CasesTest, 3D_TFSF_Upwind)
 
 TEST_F(CasesTest, 3D_NearToFarField_Upwind)
 {
-	std::string case_data{ "3D_NearToFarField" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "3D_NearToFarField" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 3D_NearToFarFieldSmaller_Upwind)
 {
-	std::string case_data{ "3D_NearToFarFieldSmaller" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "3D_NearToFarFieldSmaller" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
 
 TEST_F(CasesTest, 3D_NTFF_Sphere_Upwind)
 {
-	std::string case_data{ "3D_RCS" };
-	auto solver{ buildSolver(case_data) };
+	std::string case_name{ "3D_RCS" };
+	auto solver{ buildSolver(maxwellCase(case_name)) };
 
 	solver.run();
 }
