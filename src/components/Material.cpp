a4520d434930af4c7952714685d6569ff01a21ff
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2024-08-21 11:32:39 +0800

    Adds sigma to Materials

--- src/components/Material.cpp
+++ src/components/Material.cpp
@@ -4,21 +4,42 @@
 
 namespace maxwell {
 
-Material::Material(double epsilon, double mu) :
-	epsilon_(epsilon),
-	mu_(mu)
+void verifyParameters(double epsilon, double mu) 
 {
-	if (epsilon_ < 1.0) {
+	if (epsilon < 1.0) {
 		throw std::runtime_error("Permittivity under 1.0 not allowed.");
 	}
-	if (mu_ < 1.0) {
+	if (mu < 1.0) {
 		throw std::runtime_error("Permeability under 1.0 not allowed.");
 	}
 }
 
+void verifyParameters(double epsilon, double mu, double sigma) 
+{
+	verifyParameters(epsilon, mu);
+	if (sigma < 0.0) {
+		throw std::runtime_error("Conductivity under 0.0 is not allowed.");
+	}
+}
+
+Material::Material(double epsilon, double mu) :
+	epsilon_(epsilon),
+	mu_(mu)
+{
+	verifyParameters(epsilon, mu);
+}
+
+Material::Material(double epsilon, double mu, double sigma) :
+	epsilon_(epsilon),
+	mu_(mu),
+	sigma_(sigma)
+{
+	verifyParameters(epsilon, mu, sigma);
+}
+
 Material buildVacuumMaterial()
 {
-	return Material(1.0, 1.0);
+	return Material(1.0, 1.0, 0.0);
 }
 
 }
--- test/maxwell/components/MaterialTest.cpp
+++ test/maxwell/components/MaterialTest.cpp
@@ -31,3 +31,15 @@ TEST_F(MaterialTest, invalidEpsOrMu)
 	EXPECT_ANY_THROW(Material(-20.0,   1.0));
 	EXPECT_ANY_THROW(Material(-10.0, -10.0));
 }
+
+TEST_F(MaterialTest, electricalConductivity)
+{
+	Material vacuum{ buildVacuumMaterial() };
+	EXPECT_EQ(0.0, vacuum.getConductivity());
+}
+
+TEST_F(MaterialTest, invalidSigma)
+{
+	EXPECT_ANY_THROW(Material(1.0, 2.0, -0.5 ));
+	EXPECT_ANY_THROW(Material(2.0, 3.0, -5e-8));
+}
