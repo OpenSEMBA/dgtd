a312ed53ab941a8d95930be562c7446a0d466dc0
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2024-12-02 11:47:07 +0100

    Adds HesthavenJumpIntegrator

--- src/mfemExtension/BilinearIntegrators.h
+++ src/mfemExtension/BilinearIntegrators.h
@@ -252,5 +252,26 @@ private:
 	Vector shape1_;
 };
 
+class HesthavenFluxIntegrator : public BilinearFormIntegrator
+{
+
+public:
+	HesthavenFluxIntegrator(double b)
+	{
+		beta = b;
+	}
+
+	void AssembleFaceMatrix(const FiniteElement& el1,
+		const FiniteElement& el2,
+		FaceElementTransformations& Trans,
+		DenseMatrix& elmat);
+
+protected:
+	double beta;
+
+private:
+	Vector shape1_, shape2_;
+};
+
 }
 }
