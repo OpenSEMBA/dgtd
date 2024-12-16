245c2fbfb4934c17e8a194e773b3a9a61b19a4d5
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2024-12-16 12:33:39 +0100

    removes norm from custom comparison method

--- src/evolution/EigenExtension.cpp
+++ src/evolution/EigenExtension.cpp
@@ -6,10 +6,6 @@ namespace maxwell {
 
         bool operator()(const Eigen::MatrixXd& lhs, const Eigen::MatrixXd& rhs) const {
             
-            if (lhs.norm() != rhs.norm()) {
-                return lhs.norm() < rhs.norm();  
-            }
-            
             for (int i{ 0 }; i < lhs.rows(); i++) {
                 for (int j{ 0 }; j < lhs.cols(); j++) {
                     if (lhs(i, j) != rhs(i, j)) {
