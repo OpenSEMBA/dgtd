0a4c67071e3b1c12d17de4318eb9ec90a921b573
Alejandro Muñoz Manterola <84183271+AlejandroMunozManterola@users.noreply.github.com>
2025-01-20 10:35:28 +0100

    Adds some unitary tests to document the intended SubMesher behaviour

--- src/components/SubMesher.h
+++ src/components/SubMesher.h
@@ -94,4 +94,15 @@ private:
 
 };
 
+FaceElementTransformations* getFaceElementTransformation(Mesh& m, int be);
+Vector getBarycenterOfElement(Mesh& m, int e);
+Vector getBarycenterOfFaceElement(Mesh& m, int f);
+Vector getNormal(FaceElementTransformations& fet);
+std::pair<double, double> calculateBaryNormalProduct(Mesh& m, FaceElementTransformations& fet, int be);
+double calculateCrossBaryVertexSign(Mesh& m, FaceElementTransformations& fet, int be);
+const Vector calculateTangent2D(Mesh& m, int be);
+const Vector calculateNormal3D(Mesh& m, int be);
+const std::pair<Vector, Vector> calculateBarycenters(Mesh& m, int be);
+const Vector calculateBarycenterVector(Mesh& m, int be);
+
 };
