c9fdb3c0a0cbbcc515c5db5f5261c33334b43e1d
Alejandro M. <alevale71@gmail.com>
2025-10-24 12:16:30 +0200

    Changes solo-material with two tags into two sbc materials with individual tags, adapts tests

--- test/maxwell/solver/ExtensionTest.cpp
+++ test/maxwell/solver/ExtensionTest.cpp
@@ -31,20 +31,30 @@ TEST_F(SolverExtensionTest, isCorrect_SBC_Properties)
     auto case_data = parseJSONfile(maxwellCase("2D_InteriorBoundary_SBC_Test"));
     auto global_solver = driver::buildSolver(case_data, maxwellCase("2D_InteriorBoundary_SBC_Test"), true);
     for (auto props : global_solver.getSolverOptions().sbc_props){
-        ASSERT_EQ(1e-3, props.material_width);
-        ASSERT_EQ(12, props.num_of_segments);
-        ASSERT_EQ(3, props.order);
-    }
-
-    const auto& tag2bdr = global_solver.getModel().getGeomTagToIntBoundaryCond();
-    for(const auto& [tag, cond] : tag2bdr){
-        if (cond == BdrCond::SBC){
-            const auto sbc_material = global_solver.getModel().getGeomTagToBoundaryMaterial().at(tag);
-            ASSERT_EQ(1e5, sbc_material.getConductivity());
+        if (props.phys_tag == 15){
+            ASSERT_EQ(1e-5, props.material_width);
+            ASSERT_EQ(15, props.num_of_segments);
+            ASSERT_EQ(5, props.order);
+            ASSERT_EQ(1e5, props.material.getConductivity());
+        }
+        else if (props.phys_tag == 18){
+            ASSERT_EQ(1e-8, props.material_width);
+            ASSERT_EQ(18, props.num_of_segments);
+            ASSERT_EQ(8, props.order);
+            ASSERT_EQ(1e8, props.material.getConductivity()); // High only so it ends in 8, for 'same-number' easy checks.
         }
     }
+}
 
-
+TEST_F(SolverExtensionTest, targetIds)
+{
+    size_t order = 2;
+    size_t num_of_segments = 10;
+    std::vector<NodeId> ids = buildTargetNodeIds(order, num_of_segments);
+    ASSERT_EQ(2, ids[0]);
+    ASSERT_EQ(3, ids[1]);
+    ASSERT_EQ(33, ids[2]);
+    ASSERT_EQ(34, ids[3]);
 }
 
 TEST_F(SolverExtensionTest, buildTest)
